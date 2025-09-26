0fef7b5aeefa696249f23d850bac03793534444d
ShubhGithub1952 <105335350+ShubhGithub1952@users.noreply.github.com>
2025-09-26 19:17:39 +0530

    Update MagicTesting.java
    Added the logic for the Swaping the value of a and B

--- src/test/java/com/tutorialsninja/qa/testcases/MagicTesting.java
+++ src/test/java/com/tutorialsninja/qa/testcases/MagicTesting.java
@@ -4,7 +4,9 @@ public class MagicTesting {
 	public static void main(String[] args) {
 		int a=10;
 		int b=20;
-		a=a+b;
+		a=a+b; //a=30
+		b=a-b;//b=10
+		a=a-b;//a=20
 		System.out.println(a);
 	}
 }
