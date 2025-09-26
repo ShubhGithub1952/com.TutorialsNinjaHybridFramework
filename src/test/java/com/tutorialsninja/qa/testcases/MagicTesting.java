9943aec856688ad50ca1af54ad67fce9e0a0d0de
shubham <pitleshubham14@gmail.com>
2025-09-26 18:56:04 +0530

    Added the New class as new requirement received.

---
+++ src/test/java/com/tutorialsninja/qa/testcases/MagicTesting.java
@@ -0,0 +1,10 @@
+package com.tutorialsninja.qa.testcases;
+
+public class MagicTesting {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		a=a+b;
+		System.out.println(a);
+	}
+}
